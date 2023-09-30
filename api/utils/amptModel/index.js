ba4b41f7f4b51b0fb19237436157cbc111181759
roblesisaac <irobles1030@gmail.com>
2023-09-29 17:23:33 -0700

    cloud update

--- api/utils/amptModel/index.js
+++ api/utils/amptModel/index.js
@@ -7,6 +7,34 @@ const amptModel = function(collectionName, schemaConfig, globalConfig) {
   const labelsMap = LabelsMap(collectionName, schemaConfig);
   const schema = extractSchema(schemaConfig);
 
+  function buildId(yyyymmdd) {
+    const random = Math.random().toString(16).substring(2);
+    return `${generateDate(yyyymmdd)}_${random}`;
+  }
+
+  function buildSchema_Id() {
+    return `${collectionName}:${buildId()}`;
+  }
+
+  async function checkForDuplicates(uniqueFieldsToCheck, labelsMap, validated, collectionName) {
+    for(const uniqueField of uniqueFieldsToCheck) {
+          
+      if(!labelsMap.hasLabel(uniqueField)) {
+        throw new Error(`Unique field '${uniqueField}' for '${collectionName}' must be labeled in a labelsConfig...`);
+      }
+
+      const duplicate = await findOne({ [uniqueField]: validated[uniqueField] });
+
+      if(validated._id === duplicate?._id) {
+        continue;
+      }
+
+      if(!!duplicate) {
+        throw new Error(`Duplicate value for '${uniqueField}' exists in collection '${collectionName}'`);
+      }
+    }
+  }
+
   function extractSchema(schemaConfig) {
     const schema = {};
 
@@ -21,82 +49,100 @@ const amptModel = function(collectionName, schemaConfig, globalConfig) {
     return schema;
   }
 
-  const validate = async (dataToValidate, action) => {
+  async function find(filter, options) {
+    if(typeof filter === 'string') {
+      return await data.get(filter, options);
+    };
+
+    if(!isObject(filter)) {
+      throw new Error('Filter must be an object');
+    };
+
+    const { labelNumber, labelValue } = labelsMap.getArgumentsForGetByLabel(filter);
+    const { items,lastKey, next } = await data.getByLabel(labelNumber, labelValue);
+    const validatedResponse = [];
+
+    for(const responseItem of items) {
+      const { validated } = await validate(responseItem.value, 'get');
+      validatedResponse.push({ _id: responseItem.key, ...validated });
+    }
+
+    return { items: validatedResponse, lastKey, next };
+  }
+
+  async function findOne(filter) {
+    const response = await find(filter, { limit: 1 });
+    return response?.items?.[0];
+  }
+
+  function isObject(value) {
+    return typeof value === 'object' && !Array.isArray(value);
+  }
+
+  async function save(value) {
+    const { validated, uniqueFieldsToCheck } = await validate(value, 'set');
+
+    if (!validated) {
+      throw new Error('Validation failed');
+    }
+
+    if(uniqueFieldsToCheck.length) {
+      await checkForDuplicates(uniqueFieldsToCheck, labelsMap, validated, collectionName);
+    }
+
+    const createdLabels = await labelsMap.createLabelKeys(validated);
+    const _id = value._id || buildSchema_Id();      
+    const saved = await data.set(_id, validated, { ...createdLabels });
+
+    return { _id, ...saved };
+  }
+
+  async function update(filter, updates) {
+    const existingItem = await findOne(filter);
+
+    if(!existingItem) {
+      throw new Error('Item not found');
+    }
+
+    const { validated, uniqueFieldsToCheck } = await validate({ ...existingItem, ...updates });
+
+    if(uniqueFieldsToCheck.length) {
+      await checkForDuplicates(uniqueFieldsToCheck, labelsMap, { _id: existingItem._id, ...validated }, collectionName);
+    }
+
+    const createdLabels = await labelsMap.createLabelKeys(validated);
+    const updated = await data.set(existingItem._id, { ...existingItem, ...validated }, createdLabels);
+
+    return updated;
+  }
+
+  async function validate(dataToValidate, action) {
     return await validator(schema, dataToValidate, { globalConfig, action });
   };
-
-  function buildId(yyyymmdd) {
-    const random = Math.random().toString(16).substring(2);
-    return `${generateDate(yyyymmdd)}_${random}`;
-  }
-
-  function buildSchema_Id() {
-    return `${collectionName}:${buildId()}`;
-  }
-
-  function isSearchingBy_Id(filter) {
-    return typeof filter === 'string' && filter.includes(':');
-  }
 
   return {
     validate,
     labelsMap,
-    save: async function (value) {
-      const { validated, uniqueFieldsToCheck } = await validate(value, 'set');
-
-      if (!validated) {
-        throw new Error('Validation failed');
-      }
-
-      if(uniqueFieldsToCheck) {
-        for(const uniqueField of uniqueFieldsToCheck) {
-          
-          if(!labelsMap.hasLabel(uniqueField)) {
-            throw new Error(`Unique field '${uniqueField}' for '${collectionName}' must be labeled in a labelsConfig...`);
-          }
-
-          const duplicate = await this.find({ [uniqueField]: validated[uniqueField] });
-
-          console.log({ duplicate });
-
-          if(duplicate?.items?.length) {
-            throw new Error(`Duplicate value for '${uniqueField}'`);
-          }
-        }
-      }
-
-      const createdLabels = await labelsMap.createLabelKeys(validated);
-      const _id = value._id || buildSchema_Id();
-      
-      return await data.set(_id, { ...validated, _id }, { ...createdLabels });
-    },
-    find: async (filter, options) => {
-      if(isSearchingBy_Id(filter)) {
-        return await data.get(filter, options);
-      };
-
-      const { labelNumber, labelValue } = labelsMap.getArgumentsForGetByLabel(filter);
-      const response = await data.getByLabel(labelNumber, labelValue);
-
-      if(!response?.items?.length) {
-        return null;
-      }
-
-      const validatedResponse = [];
-
-      for(const responseItem of response.items) {
-        const { validated } = await validate(responseItem, 'get');
-        validatedResponse.push(validated);
-      }
-
-      return validatedResponse;
-    },
-    remove: async (filter) => { 
+    save,
+    find,
+    findOne,
+    update,
+    erase: async function(filter) { 
       if(typeof filter === 'string') {
         return await data.remove(filter);
       }
 
-      const itemToRemove = await labelsMap.find(filter);
+      const { _id } = await findOne(filter);
+
+      if(!_id) {
+        throw new Error(`Item not found when trying to perfomr erase in collection '${collectionName}: `);
+      }
+
+      const isRemoved = await data.remove(_id);
+      
+      return {
+        removed: isRemoved
+      }
     }
   };
 };
--- api/utils/amptModel/tests/amptModel.test.js
+++ api/utils/amptModel/tests/amptModel.test.js
@@ -10,14 +10,14 @@ describe('amptModels', () => {
     },
     createdAt: {
       set: () => Date.now(),
-      get: ({ item }) => `${item.name} created at ${item.createdAt}`
+      get: ({ item }) => `${item.name} who is ${item.age} created at ${item.createdAt}`
     },
     age: Number,
     role: {
       type: String,
-      computed: ({ item }) => item?.req?.user?.role,
+      set: ({ item }) => item?.req?.user?.role,
       default: 'user',
-      enum: ['user', 'admin', null]
+      enum: ['user', 'admin']
     }
   };
 
@@ -32,13 +32,9 @@ describe('amptModels', () => {
   };
 
   const globalConfig = { lowercase: true, trim: true };
+  const collectionName = 'testcollection';
 
-  const TestModel = amptModel('testcollection', { ...schema, ...labelsConfig }, globalConfig);
-
-  const testItem = {
-    name: 'John  ',
-    age: '20'
-  };
+  const TestModel = amptModel(collectionName, { ...schema, ...labelsConfig }, globalConfig);
 
   test('amptModel', async () => {
     expect(amptModel).toBeDefined();
@@ -57,18 +53,62 @@ describe('amptModels', () => {
   });
 
   test('ampModel.save works', async () => {
+    const testItem = { name: 'John  ', age: '20' };
     const testProps = { req: { user: { role: 'admin' } } };
     const response = await TestModel.save({ ...testItem, ...testProps }, testProps);
 
     expect(response._id).toMatch(/^testcollection/);
+    expect(response.name).toBe('john');
+    expect(response.age).toBe(20);
+    expect(response.role).toBe('admin');
   }, 20000);
 
-  test('amptModel.find works', async () => {
+  test('amptModel.find whent the filter is a string works', async () => {
+    const responseForFilterString = await TestModel.find('plaidtransactions:2023-09-06T15:35:57Z_c1c5d8c3b1de8');
+
+    expect(responseForFilterString).toBeDefined();
+  }, 1000*10);
+
+  test('amptModel.find works for user_details', async () => {
     const response = await TestModel.find({ user_details: 'jo' });
 
-    console.log({response})
-
-    expect(response).toBeDefined();
+    expect(response.items[0].name).toBe('john');
   }, 1000*10);
+
+  test('amptModel.find works for name', async () => {
+    const response = await TestModel.find({ name: 'john' });
+
+    expect(response.items[0].name).toBe('john');
+  }, 1000*10);
+
+  test('amptModel.findOne works for name', async () => {
+    const response = await TestModel.findOne({ name: 'john' });
+
+    expect(response.name).toBe('john');
+  }, 1000*10);
+
+  test('amptModel.updateWorks', async () => {
+    const response = await TestModel.update({ name: 'john' }, { age: 30 });
+
+    expect(response.age).toBe(30);
+  });
+
+  test('amptModel.findOne after update workd', async () => {
+    const response = await TestModel.findOne({ name: 'john' });
+
+    console.log({ response });
+    expect(response.age).toBe(30);
+  });
+
+  test('amptModel.save duplicate unique key throws error', async () => {
+    expect(async () => await TestModel.save({ name: 'john' }))
+      .rejects.toThrowError(`Duplicate value for 'name' exists in collection '${collectionName}'`);
+  }, 1000*10);
+
+  test('amptModel.erase works', async () => {
+    const response = await TestModel.erase({ name: 'john' });
+
+    expect(response).toEqual({ removed: true });
+  });
 
 });
