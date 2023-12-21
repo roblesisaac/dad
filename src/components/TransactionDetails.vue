7c977c2c53c3035f2ec8e2643d9ce4b5842c4af2
irobles1030 <irobles1030@gmail.com>
2023-12-21 01:44:00 +0000

    cloud update

--- src/components/TransactionDetails.vue
+++ src/components/TransactionDetails.vue
@@ -4,19 +4,66 @@
     <div class="cell-1 proper">
       <p>
         <b>Category:</b>
-        <br />{{ prettyCategory }}</p>
+        <br />{{ prettyCategory }}
+      </p>
       <p>
-        <b>Paid:</b> 
+        <b>Paid:</b>
         <br />{{ item.payment_channel }}
       </p>
       <p>
-        <b>Status</b> <span v-if="item.pending">Pending</span><span v-else>Settled</span>
+        <b>Status:</b> <span v-if="item.pending">Pending</span><span v-else>Settled</span>
       </p>
     </div>
 
     <!-- Transaction Controls -->
     <div class="cell-1">
-      
+
+      <!-- Recategorize As -->
+      <div class="grid p20b">
+        <div class="cell-1">
+          <b>Recategorize As:</b>
+        </div>
+        <div class="cell-1">
+          <input type="text" />
+        </div>
+
+        <div class="cell-1 p10t">
+          <b>Apply To:</b>
+        </div>
+        <div class="cell-1">
+          <!-- This item only -->
+          <div class="grid">
+            <div class="cell shrink">
+              <input type="radio" id="this-item-only" name="apply-to" value="this-item-only" checked>
+            </div>
+            <div class="cell auto p10l">
+              <label for="this-item-only">This item only</label>
+            </div>
+          </div>
+
+          <!-- Anything that matches -->
+          <div class="grid">
+            <div class="cell shrink">
+              <input type="radio" id="anything-that-matches" name="apply-to" value="anything-that-matches">
+            </div>
+            <div class="cell auto p10l">
+              <label for="anything-that-matches">Anything that matches</label>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Add Note -->
+      <div class="grid">
+        <div class="cell-1">
+          <b>Add Note:</b>
+        </div>
+        <div class="cell-1">
+          <textarea rows="3" class="add-note"></textarea>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -28,6 +75,8 @@ const { item } = defineProps({
   item: Object
 });
 
+console.log(item);
+
 const prettyCategory = computed(() => {
   const category = item.category || '';
 
@@ -35,3 +84,11 @@ const prettyCategory = computed(() => {
 });
 
 </script>
+
+<style>
+.add-note {
+  width: 100%;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
