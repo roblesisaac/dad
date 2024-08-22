778db45858d188180f5eb725a41ad6b606bd21b3
irobles1030 <irobles1030@gmail.com>
2024-08-22 00:52:19 +0000

    misc sticky/ scroll changes

--- src/components/CategoryDetails.vue
+++ src/components/CategoryDetails.vue
@@ -121,8 +121,7 @@ function scrollToElement(id) {
   const absoluteElementTop = elementRect.top + scrollY;
   
   window.scrollTo({
-    top: absoluteElementTop - headerHeight,
-    behavior: 'smooth'
+    top: absoluteElementTop - headerHeight
   });
 }
 
