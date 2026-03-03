4a4d82ceabf02f94392aa44dc5baf9000a4dedc1
superbasicman <superbasicman@gmail.com>
2026-03-03 13:40:45 -0800

    style: reduce ThemeCycleButton size.

--- src/shared/components/ThemeCycleButton.vue
+++ src/shared/components/ThemeCycleButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="w-6 h-6 rounded-full transition-colors focus:outline-none"
+    class="w-4 h-4 rounded-full transition-colors focus:outline-none"
     :style="{ backgroundColor: 'var(--theme-text)' }"
     :aria-label="`Current theme ${resolvedTheme}. Click to switch theme.`"
     @click="cycleTheme"
